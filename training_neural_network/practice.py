48eab6005d3627d6c7acd44a9a3693fa055db0bf
Alireza Shamsoshoara <alireza.shamsoshoara@vw.com>
2023-03-20 16:05:10 -0700

    update on practice

--- training_neural_network/practice.py
+++ training_neural_network/practice.py
@@ -15,6 +15,18 @@ import torch.nn as nn
 #########################################################
 # Function definition
 
+def test_activation():
+    """_summary_
+    """
+    activation_layer = nn.Sigmoid()
+    x = torch.rand(2, 10)
+    y_sig = activation_layer(x)
+    print(f"input _x = {x}")
+    print(f"output _y_sig = {y_sig}")
+
+    y_tan = nn.Tanh()(x)
+    print(f"output _y_tan = {y_tan}")
+
 
 if __name__ == "__main__":
-    pass
+    test_activation()
