dd32da3e6a2f641bcd89acbff38fef50a83847f1
Alireza Shamsoshoara <alireza.shamsoshoara@vw.com>
2023-03-20 16:09:19 -0700

    update training

--- training_neural_network/practice.py
+++ training_neural_network/practice.py
@@ -18,14 +18,17 @@ import torch.nn as nn
 def test_activation():
     """_summary_
     """
+    x = torch.rand(2, 10) - 0.5
     activation_layer = nn.Sigmoid()
-    x = torch.rand(2, 10)
     y_sig = activation_layer(x)
     print(f"input _x = {x}")
     print(f"output _y_sig = {y_sig}")
 
     y_tan = nn.Tanh()(x)
     print(f"output _y_tan = {y_tan}")
+    
+    y_relu = nn.ReLU()(x)
+    print(f"output _y_relu = {y_relu}")
 
 
 if __name__ == "__main__":
