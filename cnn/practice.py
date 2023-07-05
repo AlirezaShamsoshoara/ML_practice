bbf58c121bb798a3eec51caf6e54ba4dbc5adc2b
Alireza <a.shamsoshoara@gmail.com>
2023-07-04 22:39:56 -0700

    build cnn

--- cnn/practice.py
+++ cnn/practice.py
@@ -17,5 +17,12 @@ import torch.nn as nn
 # Function definition
 
 
+def test_cnn():
+    input_img = torch.rand(1,3,7,7)
+    layer = nn.Conv2d(in_channels=3, out_channels=6, kernel_size=3, stride=2, padding=1)
+    out = layer(input_img)
+    print(out.shape)
+
+
 if __name__ == "__main__":
-    test_activation()
+    test_cnn()
