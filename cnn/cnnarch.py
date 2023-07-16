00a47e41ede7a5eb762749da6d6e9d11f066424a
Alireza <a.shamsoshoara@gmail.com>
2023-07-16 14:54:07 -0700

    finish cnn and start rnn

--- cnn/cnnarch.py
+++ cnn/cnnarch.py
@@ -57,8 +57,52 @@ class AlexNet(nn.Module):
         x = self.classifier(x)
         return x
 
+class InceptionModule(nn.Module):
+    """_summary_
+    In general, a larger kernel is preferred for information that resides globally,
+    and a smaller kernel is preferred for information that is distributed locally.
+    Args:
+        nn (_type_): _description_
+    """
+    def __init__(self, in_channels, out_channels):
+        super(InceptionModule, self).__init__()
+        relu = nn.ReLU()
+        self.branch1 = nn.Sequential(
+            nn.Conv2d(in_channels=in_channels, out_channels=out_channels, kernel_size=1, stride=1, padding=0),
+            relu
+        )
+        conv3_1 = nn.Conv2d(in_channels=in_channels, out_channels=out_channels, kernel_size=1, stride=1, padding=0)
+        conv3_3 = nn.Conv2d(in_channels=out_channels, out_channels=out_channels, kernel_size=3, stride=1, padding=1)
+        self.branch2 = nn.Sequential(conv3_1, conv3_3, relu)
+
+        conv5_1 = nn.Conv2d(in_channels=in_channels, out_channels=out_channels, kernel_size=1, stride=1, padding=0)
+        conv5_5 = nn.Conv2d(in_channels=out_channels, out_channels=out_channels, kernel_size=5, stride=1, padding=2)
+        self.branch3 = nn.Sequential(conv5_1, conv5_5, relu)
+
+        max_pool_1 = nn.MaxPool2d(kernel_size=3, stride=1, padding=1)
+        conv_last_1 = nn.Conv2d(in_channels=in_channels, out_channels=out_channels, kernel_size=1, stride=1, padding=0)
+        self.branch4 = nn.Sequential(max_pool_1, conv_last_1, relu)
+
+    def forward(self, x):
+        """_summary_
+
+        Args:
+            x (_type_): _description_
+
+        Returns:
+            _type_: _description_
+        """
+        output_1 = self.branch1(x)
+        output_2 = self.branch2(x)
+        output_3 = self.branch3(x)
+        output_4 = self.branch4(x)
+        return torch.cat([output_1, output_2, output_3, output_4])
+    
 
 if __name__ == "__main__":
-    model = AlexNet()
+    model_alexnet = AlexNet()
     inp = torch.rand(1, 3, 128, 128)
-    print(model(inp).shape)
+    print(model_alexnet(inp).shape)
+
+    model_inception = InceptionModule(in_channels=3, out_channels=32)
+    print(model_inception(inp).shape)
---
+++ rnn/simplernn.py
@@ -0,0 +1,23 @@
+'''
+#################################
+# Python API: ML Practice (RNN Simple)
+#################################
+'''
+
+#########################################################
+# import libraries
+import torch
+import torch.nn as nn
+
+#########################################################
+# General Parameters
+
+
+#########################################################
+# Function definition
+
+"""_summary_
+As we already know, convolutional layers are specialized for processing grid-structured data (i.e., images). On the contrary, recurrent layers are designed for processing sequences.
+
+To distinguish Recurrent NN (RNNs) from fully-connected layers, we call the non-recurrent networks feedforward NN.
+"""
