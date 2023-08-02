feedc868767c23c7e645e0137213a07b03b94f14
Alireza <a.shamsoshoara@gmail.com>
2023-08-01 23:00:28 -0700

    update on LSTM

--- rnn/samplelstm.py
+++ rnn/samplelstm.py
@@ -24,7 +24,9 @@ class MyLstmCell(torch.nn.Module):
 
         # forget gate components
         # 1. DEFINE FORGET GATE COMPONENTS
-
+        self.linear_gate_w1 = nn.Linear(self.input_length, self.hidden_length, bias=True)
+        self.linear_gate_r1 = nn.Linear(self.hidden_length, self.hidden_length, bias=False)
+        self.sigmoid_forget = nn.Sigmoid()
 
         # input gate components
         self.linear_gate_w2 = nn.Linear(self.input_length, self.hidden_length, bias=True)
@@ -33,17 +35,27 @@ class MyLstmCell(torch.nn.Module):
 
         # cell memory components
         # 2. DEFINE CELL MEMORY COMPONENTS
+        self.linear_gate_w3 = nn.Linear(self.input_length, self.hidden_length, bias=True)
+        self.linear_gate_r3 = nn.Linear(self.hidden_length, self.hidden_length, bias=False)
+        self.activation_gate = nn.Tanh()
 
 
         # out gate components
         # 3. DEFINE OUT GATE COMPONENTS
+        self.linear_gate_w4 = nn.Linear(self.input_length, self.hidden_length, bias=True)
+        self.linear_gate_r4 = nn.Linear(self.hidden_length, self.hidden_length, bias=False)
+        self.sigmoid_hidden_out = nn.Sigmoid()
 
 
         # final output
         # 4. DEFINE OUTPUT
+        self.activation_final = nn.Tanh()
 
     def forget(self, x, h):
-        pass
+        x_temp = self.linear_gate_w1(x)
+        h_temp = self.linear_gate_r1(h)
+        f = self.sigmoid_forget(x_temp + h_temp)
+        return f
 
     def input_gate(self, x, h):
         # input gate
@@ -53,10 +65,22 @@ class MyLstmCell(torch.nn.Module):
         return i
 
     def cell_memory_gate(self, i, f, x, g, c_prev):
-        pass
+        x = self.linear_gate_w3(x)
+        h = self.linear_gate_r3(h)
+
+        # new information part that will be injected in the new context
+        k = self.activation_gate(x + h)
+        g = k * i
+        # forget old context/cell info
+        c = f * c_prev
+        # learn new context/cell info
+        c_next = g + c
+        return c_next
 
     def out_gate(self, x, h):
-        pass
+        x = self.linear_gate_w4(x)
+        h = self.linear_gate_r4(h)
+        return self.sigmoid_hidden_out(x + h)
 
     def forward(self, x, tuple_in ):
         (h, c_prev) = tuple_in
@@ -77,9 +101,15 @@ class MyLstmCell(torch.nn.Module):
 
         return h_next, c_next
 
+
 def lstm_test():
+    """_summary_
+    """
     layer = nn.LSTM(input_size=10, hidden_size=20, num_layers=2)
-    
+    return layer
+
 
 if __name__ == "__main__":
-    lstm_test()
+    layer = lstm_test()
+    lst_obj = MyLstmCell()
+    print(lst_obj)
