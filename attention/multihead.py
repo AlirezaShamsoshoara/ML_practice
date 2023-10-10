ca6c5e78943984c9bf38076df92243d7af869c7f
Alireza <a.shamsoshoara@gmail.com>
2023-10-09 19:58:23 -0700

    update mhsa

--- attention/multihead.py
+++ attention/multihead.py
@@ -22,6 +22,7 @@ import torchvision.transforms as transforms
 from torchvision import datasets
 import torch.optim as optim
 from copy import deepcopy
+from attention.simpleattention import ScaledDotProductAttention
 
 #########################################################
 # General Parameters
@@ -47,9 +48,41 @@ class MultiHeadSelfAttention(nn.Module):
         self.linears = nn.ModuleList(
             [deepcopy(nn.Linear(in_features=d_model, out_features=d_model)) for _ in range(4)]
         )
+        
+        self.spda = ScaledDotProductAttention()
+        self.attn = None
+        self.dropout = nn.Dropout(p=dropout)
 
-    def forward(self, ):
-        pass
+    def forward(self, query: torch.FloatTensor,
+                key: torch.FloatTensor,
+                value: torch.FloatTensor,
+                mask: Optional[torch.ByteTensor] = None) -> torch.FloatTensor:
+        """_summary_
+
+        Args:
+            query (torch.FloatTensor): shape (batch_size, max_len, d_model)
+            key (torch.FloatTensor): shape (batch_size, max_len, d_model)
+            value (torch.FloatTensor): shape (batch_size, max_len, d_model)
+            mask (Optional[torch.ByteTensor], optional): (batch_size, max_len)
+
+        Returns:
+            shape (batch_size, max_len, d_model)
+        """
+        if mask is not None:
+            mask = mask.unsqueeze(1).unsqueeze(1)
+
+        batch_size = query.size(0)
+        # 1) Do all the linear projections in batch from d_model => h x d_k
+        query, key, value = [l(x).view(batch_size, -1, self.h, self.d_k).transpose(1, 2)
+                             for l, x in zip(self.linears, (query, key, value))]
+        
+        # 2) Apply attention on all the projected vectors in batch.
+        # x: B x H x L x D_v
+        x, self.attn = self.sdpa(query, key, value, mask=mask, dropout=self.dropout)
+        
+        # 3) "Concat" using a view and apply a final linear.
+        x = x.transpose(1, 2).contiguous().view(batch_size, -1, self.h * self.d_k)
+        return self.linears[-1](x)
 
 if __name__ == "__main__":
     pass
