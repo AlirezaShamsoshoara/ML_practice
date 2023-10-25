dda681d31c9adae589b37d001cbdd15c84e6cfd4
Alireza Shamsoshoara <alireza.shamsoshoara@vw.com>
2023-10-25 15:33:00 -0700

    add segmentation graph

--- graph/practice.py
+++ graph/practice.py
@@ -164,10 +164,30 @@ def test_eigenvalue():
 
 
 def segmentation_laplacian():
-    
-    
-    
+    """_summary_
+    """
+    re_size = 64 # ownsampling of resized rectangular image
+    img = misc.face(gray=True)  #retrieve a grayscale image
+    img = resize(img, (re_size, re_size))
+    mask = img.astype(bool)
+    graph = img_to_graph(img, mask=mask)
+    # Take a decreasing function of the gradient: we take it weakly
+    # dependant from the gradient the segmentation is close to a voronoi
+    graph.data = np.exp(-graph.data / graph.data.std())
+    labels = spectral_clustering(graph, n_clusters=3)
+    label_im = -np.ones(mask.shape)
+    label_im[mask] = labels
+
+    plt.figure(figsize=(6, 3))
+    plt.imshow(img, cmap='gray', interpolation='nearest')
+
+    plt.figure(figsize=(6, 3))
+    plt.imshow(label_im, cmap=plt.cm.nipy_spectral, interpolation='nearest')
+    plt.show()
+
+
 if __name__ == "__main__":
     # test_lapl()
     # test_lalp_norm()
-    test_eigenvalue()
+    # test_eigenvalue()
+    segmentation_laplacian()
